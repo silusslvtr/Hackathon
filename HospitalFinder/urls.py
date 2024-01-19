638acd76c25b66dc01fc48348b501cced2fa4c11
silusslvtr <silus.slvtr2020@gmail.com>
2024-01-19 19:25:52 +0545

    Included base, header and footer file

--- HospitalFinder/urls.py
+++ HospitalFinder/urls.py
@@ -17,6 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HOME, name='home'),
 ]
